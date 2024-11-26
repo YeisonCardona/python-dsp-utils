706dfc0de6494fb8ef691e88c3cada0906498175
Yeison Cardona <yencardonaal@unal.edu.co>
2024-11-26 08:59:37 -0500

    Added filters

--- dsp_utils/signals/generator.py
+++ dsp_utils/signals/generator.py
@@ -12,7 +12,7 @@ class SignalGenerator:
     duration: float
     sampling_rate: float
     phase: float = 0
-
+    time_endpoint: bool = False
 
     # ----------------------------------------------------------------------
     def __post_init__(self):
@@ -25,7 +25,7 @@ class SignalGenerator:
                 if isinstance(f, Iterable):
                     frequency.append(np.linspace(f[0], f[1], n))
                 else:
-                    frequency.append(np.array([f]*n))
+                    frequency.append(np.array([f] * n))
             self.frequency = np.array(frequency)
 
         if isinstance(self.amplitude, Iterable):
@@ -35,9 +35,12 @@ class SignalGenerator:
     @property
     def time(self):
         """"""
-        #return np.arange(0, self.duration, 1/self.sampling_rate)
-        return np.linspace(0, self.duration, self.duration*self.sampling_rate, endpoint=True)
-
+        return np.linspace(
+            0,
+            self.duration,
+            self.duration * self.sampling_rate,
+            endpoint=self.time_endpoint,
+        )
 
     # ----------------------------------------------------------------------
     @property
@@ -52,29 +55,49 @@ class SignalGenerator:
     @property
     def sine_wave(self):
         """"""
-        return self.amplitude * np.sin(2 * np.pi * self.frequency * self.time_tile + self.phase)
+        return self.amplitude * np.sin(
+            2 * np.pi * self.frequency * self.time_tile + self.phase
+        )
 
     # ----------------------------------------------------------------------
     @property
     def square_wave(self):
         """"""
-        return self.amplitude * signal.square(2 * np.pi * self.frequency * self.time_tile + self.phase)
+        return self.amplitude * signal.square(
+            2 * np.pi * self.frequency * self.time_tile + self.phase
+        )
 
     # ----------------------------------------------------------------------
     @property
     def triangle_wave(self):
         """"""
-        return self.amplitude * signal.sawtooth(2 * np.pi * self.frequency * self.time_tile + self.phase, width=0.5)
+        return self.amplitude * signal.sawtooth(
+            2 * np.pi * self.frequency * self.time_tile + self.phase, width=0.5
+        )
 
     # ----------------------------------------------------------------------
     @property
     def sawtooth_wave(self):
         """"""
-        return self.amplitude * signal.sawtooth(2 * np.pi * self.frequency * self.time_tile + self.phase, width=1)
+        return self.amplitude * signal.sawtooth(
+            2 * np.pi * self.frequency * self.time_tile + self.phase, width=1
+        )
 
     # ----------------------------------------------------------------------
     def gaussian_noise(self, /, mean=0, std_dev=0.5):
         """"""
         return np.random.normal(mean, std_dev, self.time_tile.shape)
 
+    # ----------------------------------------------------------------------
+    def power(self, time_series):
+        """"""
+        return np.mean(time_series**2)
 
+    # ----------------------------------------------------------------------
+    def gaussian_noise_db(self, time_series, /, db=3):
+        """"""
+        signal_power = self.power(time_series)
+        snr_linear = 10 ** (db / 10)
+        noise_power = signal_power / snr_linear
+
+        return np.random.normal(0, np.sqrt(noise_power), time_series.shape)
